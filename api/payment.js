58a7bcaac92bcd253f13b94eb5ab81b616899551
saidulexpert-beep <saidulexpert@gmail.com>
2026-09-01 15:16:13 +0600

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -104,5 +104,4 @@ export default async function handler(req, res) {
     console.error("NagorikPay API Error:", error);
     return res.status(500).json({ error: error.message || 'Server Connection Failed' });
   }
-        }
-      
+}
